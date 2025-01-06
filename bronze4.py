ea74b4c4e4dfccf3b7475b3f31b08b3b796246e7
weonyee <weonyee@gmail.com>
2025-01-06 18:57:01 +0900

    주사위세개_브론즈4

--- bronze4.py
+++ bronze4.py
@@ -1,10 +1,13 @@
 a,b,c = map(int, input().split())
-if a==b and b==c:
+if a==b and b==c: 
     print(10000+a*1000)
+
 elif a==b or c==a:
     print(1000+ a*100)
+
 elif b==c:
     print(1000+b*100)
+    
 else: 
     if a>b and a>c:
         print(a*100)
