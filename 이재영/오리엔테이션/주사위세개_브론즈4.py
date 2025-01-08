65c02f7bc1d661e916b9618d7a5d619f6af4991c
lejy <129030474+lejy@users.noreply.github.com>
2025-01-08 09:05:05 +0900

    Update 주사위세개_브론즈4.py

--- 이재영/오리엔테이션/주사위세개_브론즈4.py
+++ 이재영/오리엔테이션/주사위세개_브론즈4.py
@@ -20,3 +20,14 @@
 예제 출력 1 
 1300
 '''
+
+num = list(map(int,input("").split()))
+b = set(num)
+
+if len(b) == 3:
+    print(max(num) * 100)
+elif len(b) == 2:
+    c = sum(num) - sum(b)
+    print(1000 + c * 100)
+else:
+    print(10000 + num.pop() * 1000)
