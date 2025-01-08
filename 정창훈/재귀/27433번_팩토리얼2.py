3663c159af3694ab27a1e6594b87b865ed7a95a7
UngSangYoon <yws804@gmail.com>
2025-01-08 09:09:39 +0900

    feat:27433_팩토리얼2_정창훈

---
+++ 정창훈/재귀/27433번_팩토리얼2.py
@@ -0,0 +1,13 @@
+# GPT 활용해서 팩토리얼문 학습을 함 
+# 프롬프트를 12살도 이해할 수 있도록 설명해줘라고 하니 잘 설명해줌
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i 
+    return result
+n = int(input("숫자 N 을 입력해주시오. (0≤ N ≤ 20): "))
+
+factorial_result = factorial(n)
+
+print(factorial_result)
