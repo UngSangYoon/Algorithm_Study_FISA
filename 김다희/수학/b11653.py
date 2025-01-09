5a53bd4c27d21ec1ea40a3290410c38a4a5c7f28
ingstats <statsxwhiz@gmail.com>
2025-01-09 17:35:07 +0900

    feat: 'b11653.py' '김다희' '수학'

---
+++ 김다희/수학/b11653.py
@@ -0,0 +1,21 @@
+# 소인수분해
+# 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+# if n == 1: pass 가 없어도 정답
+
+n = int(input())
+m = n
+list_ = [] # 약수 저장할 리스트
+if n == 1:
+    pass
+else:
+    # 약수 리스트 생성 과정
+    for i in range(2, n+1):
+        if n%i == 0:
+            list_.append(i) # 약수 i 리스트 추가
+            n = n/i
+    # 출력 과정
+    for j in range(len(list_)):
+        while m%list_[j] == 0:
+            print(list_[j])
+            m = m/list_[j]
