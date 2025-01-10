5164f7c97b3e73b7e401333a9feba902c7ffe109
jimin <nijo5093@gmail.com>
2025-01-10 17:31:31 +0900

    feat:소인수분해.py 황지민 정수론

--- 황지민/정수론/11653.py
+++ 황지민/정수론/11653.py
@@ -22,15 +22,30 @@ N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한
 
 '''
 
+
 from math import sqrt
 
-n = int(input('입력'))
+n = int(input())
 
 def p(n):
     if n < 2:
         return 0
-    for i in range(2, int(sqrt(n)) + 1):
+    for i in range(2, n + 1):
         while n % i == 0: 
             n=n/i   
             print(i)
 p(n)
+
+'''
+n = int(input())
+
+def p(n):
+    if n < 2:
+        return 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            n //= i
+            print(i)
+
+p(n)
+'''
