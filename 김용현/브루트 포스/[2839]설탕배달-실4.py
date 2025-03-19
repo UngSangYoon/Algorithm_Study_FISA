d7a077ae56e9814c920704d71d11c530fbae36ef
jamesKYH <raykim9777@gmail.com>
2025-03-19 16:19:33 +0900

    DP1주차 2주차

--- 김용현/브루트 포스/[2839]설탕배달-실4.py
+++ 김용현/브루트 포스/[2839]설탕배달-실4.py
@@ -1,26 +1,15 @@
-n = int(input())
+N, M = map(int, input().split())
+A, B = [], []
 
-# 5kg 봉지만으로 정확히 나눠떨어지는 경우
-if n % 5 == 0:
-    print(n // 5)  # 5로 나눈 몫을 출력
-else:
-    p = 0  # 봉지 개수를 세는 변수
-    while n > 0:
-        n -= 3  # 먼저 3kg 봉지를 하나 사용
-        p += 1  # 3kg 봉지 사용 개수 증가
+for _ in range(N):
+    row = list(map(int, input().split()))
+    A.append(row)
 
-        # 남은 무게가 5로 나눠떨어지는 경우
-        if n % 5 == 0:
-            p += n // 5  # 5kg 봉지 개수를 더함
-            print(p)  # 총 봉지 개수 출력
-            break
+for _ in range(N):
+    row = list(map(int, input().split()))
+    B.append(row)
 
-        # 1kg 또는 2kg이 남으면 더 이상 나눌 수 없음
-        elif n == 1 or n == 2:
-            print(-1)  # 정확히 나눌 수 없으므로 -1 출력
-            break
-
-        # 3으로 나눠떨어지는 경우
-        elif n == 0:
-            print(p)  # 총 봉지 개수 출력
-            break
+for row in range(N):
+    for col in range(M):
+        print(A[row][col] + B[row][col], end=" ")
+    print()
