fc8aa5bcad96c2c977165cd4e5695393bc861e90
UngSangYoon <yws804@gmail.com>
2025-03-23 22:07:28 +0900

    사탕게임

---
+++ 윤웅상/실전문제풀이1/2_사탕_게임_실버2_13점.py
@@ -0,0 +1,52 @@
+# 브루트 포스
+
+def get_best(y, x):
+    global N, matrix
+    best = 0
+
+    # one row
+    bef = '-'
+    value = 0
+    for j in range(N): 
+        if bef == matrix[y][j]:
+            value += 1
+        else:
+            value = 1
+        bef = matrix[y][j]
+        best = max(best, value)
+
+    # one column
+    bef = '-'
+    value = 0
+    for i in range(N): 
+        if bef == matrix[i][x]:
+            value += 1
+        else:
+            value = 1
+        bef = matrix[i][x]
+        best = max(best, value)
+
+    return best
+
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+N = int(input())
+matrix = [list(input()) for _ in range(N)]
+ans = 0
+
+for y in range(N):
+    for x in range(N):
+        if y == x:
+            ans = max(ans, get_best(y, x))
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < N and 0 <= nx < N:
+                if matrix[y][x] != matrix[ny][nx]:
+                    matrix[y][x], matrix[ny][nx] = matrix[ny][nx], matrix[y][x]
+                    ans = max(ans, get_best(y, x))
+                    matrix[y][x], matrix[ny][nx] = matrix[ny][nx], matrix[y][x]
+
+print(ans)
