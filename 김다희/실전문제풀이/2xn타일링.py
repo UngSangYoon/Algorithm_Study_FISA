8b0342ca740d109988cabf90314213f05a79cbea
ingstats <statsxwhiz@gmail.com>
2025-04-08 10:54:30 +0900

    fix: 오류 수정정

--- 김다희/실전문제풀이/2xn타일링.py
+++ 김다희/실전문제풀이/2xn타일링.py
@@ -11,12 +11,9 @@ dp = [0]*max(3, (n+1))
 dp[1] = 1
 dp[2] = 2
 
-if n > 2:
-    for i in range(3, n+1):
-        dp[i] = dp[i-1] + dp[i-2]
-        print(int(dp[n]%10007))
-else:
-    print(int(dp[n]%10007))
-
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+    
+print(int(dp[n]%10007))
 
 
