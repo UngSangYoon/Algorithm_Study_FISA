155f2e931823eb8962ff67c03eefa9ab5976b817
ingstats <statsxwhiz@gmail.com>
2025-04-08 10:52:31 +0900

    feat: '2xn타일링.py' '김다희' 'DP'

---
+++ 김다희/실전문제풀이/2xn타일링.py
@@ -0,0 +1,22 @@
+# 11726, 2×n 타일링
+# https://www.acmicpc.net/problem/11726
+
+# 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수
+# dp에서 많이 본 유형
+# dp[n] = dp[n-1] + dp[n-2]
+
+n = int(input())
+
+dp = [0]*max(3, (n+1))
+dp[1] = 1
+dp[2] = 2
+
+if n > 2:
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        print(int(dp[n]%10007))
+else:
+    print(int(dp[n]%10007))
+
+
+
